44b474a4c14d999a8a364ec4e22bf611667ec1d4
Vadim Blagiy <vadblagiy@gmail.com>
2020-10-02 11:08:52 +0300

    test task

---
+++ src/test/java/ua/test/task/controllers/SuperheroControllerTest.java
@@ -0,0 +1,13 @@
+package ua.test.task.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuperheroControllerTest {
+
+    @Test
+    public void allHero() {
+
+    }
+}
